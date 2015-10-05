56cb130ffbc5a46807c71d860ed5b156017454ec
Davide Ceolin <davide.ceolin@gmail.com>
2015-10-05 14:28:24 +0200

    fixed csv2rdf and test

--- R/rcsvw.R
+++ R/rcsvw.R
@@ -2,20 +2,21 @@ library("rrdf")
 library("RCurl")
 library("rjson")
 
-store <- new.rdf()
-add.prefix(store,prefix="csvw",namespace="http://www.w3.org/ns/csvw#")
-add.prefix(store,prefix="rdf",namespace="http://www.w3.org/1999/02/22-rdf-syntax-ns#")
-add.prefix(store,prefix="xsd",namespace="http://www.w3.org/2001/XMLSchema#")
-csvw_describes <- create.property(store,"http://www.w3.org/ns/csvw#describes")
-csvw_Table <- create.resource(store,"http://www.w3.org/ns/csvw#Table")
-csvw_tablegroup <- create.resource(store,"http://www.w3.org/ns/csvw#TableGroup")
-rdf_type <- create.property(store,"http://www.w3.org/1999/02/22-rdf-syntax-ns#type")
-csvw_Row <- create.resource(store,"http://www.w3.org/ns/csvw#Row")
-csvw_rownum <-create.property(store,"http://www.w3.org/ns/csvw#rownum")
-csvw_url<-create.property(store,"http://www.w3.org/ns/csvw#url")
-csvw_table <- create.property(store,"http://www.w3.org/ns/csvw#table")
-csvw_row <- create.property(store,"http://www.w3.org/ns/csvw#row")
-
+init<-function(){
+  store <<- new.rdf()
+  add.prefix(store,prefix="csvw",namespace="http://www.w3.org/ns/csvw#")
+  add.prefix(store,prefix="rdf",namespace="http://www.w3.org/1999/02/22-rdf-syntax-ns#")
+  add.prefix(store,prefix="xsd",namespace="http://www.w3.org/2001/XMLSchema#")
+  csvw_describes <<- create.property(store,"http://www.w3.org/ns/csvw#describes")
+  csvw_Table <<- create.resource(store,"http://www.w3.org/ns/csvw#Table")
+  csvw_tablegroup <<- create.resource(store,"http://www.w3.org/ns/csvw#TableGroup")
+  rdf_type <<- create.property(store,"http://www.w3.org/1999/02/22-rdf-syntax-ns#type")
+  csvw_Row <<- create.resource(store,"http://www.w3.org/ns/csvw#Row")
+  csvw_rownum <<-create.property(store,"http://www.w3.org/ns/csvw#rownum")
+  csvw_url<<-create.property(store,"http://www.w3.org/ns/csvw#url")
+  csvw_table <<- create.property(store,"http://www.w3.org/ns/csvw#table")
+  csvw_row <<- create.property(store,"http://www.w3.org/ns/csvw#row")
+}
 csv2json<-function(url){
   result <- read.csv(text=getURL(url,.opts=curlOptions(followlocation=TRUE)),check.names=FALSE)
   result <- lapply(rownames(result),row2json,url,result)
@@ -29,6 +30,7 @@ row2json<-function(index,url,data){
 }
 
 csv2rdf<-function(url,output="text"){
+  init()
   add.prefix(store,prefix="",namespace=paste(url,"#",sep=""))
   data <- read.csv(text=getURL(url,.opts=curlOptions(followlocation=TRUE)),check.names=FALSE,stringsAsFactors = FALSE)
   url <- tail(unlist(strsplit(url,"/")),n=1)
@@ -44,8 +46,8 @@ csv2rdf<-function(url,output="text"){
   }else if(output=="file"){
     fn<-strsplit(url,".",fixed=TRUE)[[1]][1]
     fn<-paste(fn,".ttl",sep="")
-    save.rdf(store,x,format="TURTLE")
-    print ("file saved")
+    save.rdf(store,fn,format="TURTLE")
+    print (paste(fn,"saved"))
   }else if(output=="store"){
     store
   }
@@ -58,15 +60,15 @@ row2rdf<-function(index,url,data,tb){
   add.triple(store,rowrdf,rdf_type,csvw_Row)
   desc <- create.blankNode(store)
   add.triple(store,rowrdf,csvw_describes,desc)
-  lapply(seq(1,ncol(data)),rowdescribes,data,index,desc)
+  lapply(seq(1,ncol(data)),rowdescribes,data,index,desc,url)
   add.triple(store,rowrdf,csvw_rownum,index)
   add.triple(store,rowrdf,csvw_url,paste(url,"#row=",strtoi(index)+1,sep=""))
   add.triple(store,tb,csvw_row,rowrdf)
 }
 
-rowdescribes <- function(i,data,index,desc){
+rowdescribes <- function(i,data,index,desc,url){
   p<-create.property(store,paste(url,"#",colnames(data)[i],sep=""));
   add.triple(store,desc,p,as.character(data[index,i]))
 }
 
-csv2rdf("http://www.w3.org/2013/csvw/tests/test001.csv",output = "file")
+#csv2rdf("http://www.w3.org/2013/csvw/tests/test001.csv",output = "file")
